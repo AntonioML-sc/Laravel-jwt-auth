2a56a1501d8833f75b106ccc8ecaa0c1463eda61
“AntonioML-sc” <antoniomoralesledesma@gmail.com>
2022-07-21 11:20:17 +0200

    feat: logout done

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,5 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/myProfile', [AuthController::class, 'myProfile'])->middleware('jwt.auth');
+
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('jwt.auth');;
